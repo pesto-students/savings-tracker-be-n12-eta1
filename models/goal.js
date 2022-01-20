b3f88285da4473c0f8645319582cc51dfda5bcd0
Harish patidar <patidarharish08@gmail.com>
2022-01-20 20:59:25 +0530

    added view goal api with pagination and perpage filter

--- models/goal.js
+++ models/goal.js
@@ -1,30 +1,43 @@
 import mongoose from 'mongoose'
+import mongoosePaginate from 'mongoose-paginate-v2';
 
 const Schema = mongoose.Schema;
 
 const GoalSchema = new Schema({
                                   user_id: {
-                                      type: Number,
+                                      type: String,
                                       required: [true, 'User ID field is required']
                                   },
                                   title: {
                                       type: String,
                                       required: [true, 'Title field is required']
                                   },
-                                  getStatus: {
+                                  description: {
+                                    type: String,
+                                    required: [true, 'Title field is required']
+                                },
+                                  status: {
                                       type: String,
-                                      required: [false, 'Status field is required']
+                                      required: [false, 'Status field is required'],
+                                      default: 'active'
                                   },
                                   total_amount: {
                                       type: String,
                                       required: [true, 'Total Amount field is required']
                                   },
+                                  start_date: {
+                                      type: Date,
+                                      required: [true, 'Start Date is required'],
+                                      default: new Date
+                                  },
                                   end_date: {
-                                      type: String,
+                                      type: Date,
                                       required: [true, 'End Date is required']
                                   },
                               });
 
-const User = mongoose.model('goal', GoalSchema);
+GoalSchema.plugin(mongoosePaginate);
 
-export default User;
+const Goal = mongoose.model('goal', GoalSchema);
+
+export default Goal;
--- routes/goalRoutes.js
+++ routes/goalRoutes.js
@@ -1,28 +1,11 @@
-import express from 'express'
-import Goals from '../models/goal.js'
+import express from 'express';
+import GoalController from '../controllers/goals/GoalController.js';
 
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
-    try {
-
-        Goals.find({}).then(function (goals) {
-            res.send(goals)
-        }).catch(next)
-    } catch (e) {
-        res.send({error: true, message: e.message})
-    }
-});
-
-router.post('/', function (req, res, next) {
-    try {
-        Goals.create(req.body).then(function (goal) {
-            res.send(goal);
-        }).catch(next);
-    } catch (e) {
-        res.send({error: true, message: e.message})
-    }
-
-});
+router.get('/', GoalController.getGoals);
+router.post('/', GoalController.addGoal);
+router.put('/:GoalId', GoalController.updateGoal);
+router.delete('/:GoalId', GoalController.deleteGoal);
 
 export default router
